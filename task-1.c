59f1a26a0a7605bd9ec6ade7b72b0f345c68c246
HustlerDev45 <abdoul4845@gmail.com>
2023-10-13 15:31:12 -0700

    task-1.c

---
+++ task-1.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * _printf - printf function.
+ * @format: The format string.
+ * @...: A variable number of arguments to print.
+ *
+ * Return: The number of char to print.
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int printed_chars = 0;
+
+	va_start(args, format);
+
+	while (format && *format)
+	{
+		if (*format != '%')
+		{
+			putchar(*format);
+			printed_chars++;
+		}
+		else
+		{
+			format++; // Move past '%'
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				putchar(c);
+				printed_chars++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+				{
+					putchar(*str);
+					printed_chars++;
+					str++;
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				printed_chars++;
+			}
+		}
+		format++;
+	}
+
+	va_end(args);
+
+	return (printed_chars);
+}
