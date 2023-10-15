2a26c25a9c539d82d164627245023be084e012ea
HustlerDev45 <abdoul4845@gmail.com>
2023-10-15 15:06:18 -0700

    tst-update

--- _printf_t.c
+++ _printf_t.c
@@ -1,99 +1,46 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
- * _printf - printf function.
- * @format: The format string.
- * @...: A variable number of arguments to print.
+ * _printf - Print output to stdout.
+ * @format: String
  *
- * Return: Printed chars.
+ * Return: The number of chars printed.
  */
-
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD:_printf_t.c
 	va_list args;
-	int printed_chars = 0;
-
 	va_start(args, format);
 
-	while (format && *format)
-	{
-		if (*format != '%')
-		{
-			putchar(*format);
-			printed_chars++;
-		}
-		else
-		{
-			format++; // Move past '%'
-			if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-				putchar(c);
-				printed_chars++;
+	int count = 0; // Number of characters printed
+
+	while (*format != '\0') {
+		if (*format == '%') {
+			format++; // Move to the next character after '%'
+			if (*format == '\0') {
+				break; // End of the format string, exit the loop
 			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(null)";
-				while (*str)
-				{
-					putchar(*str);
-					printed_chars++;
+			if (*format == 'c') {
+				char c = va_arg(args, int); // Get the next argument as a character
+				putchar(c); // Write the character to stdout
+				count++;
+			} else if (*format == 's') {
+				char *str = va_arg(args, char *); // Get the next argument as a string
+				while (*str) {
+					putchar(*str); // Write each character of the string to stdout
 					str++;
+					count++;
 				}
+			} else if (*format == '%') {
+				putchar('%'); // Write a literal '%' character to stdout
+				count++;
 			}
-			else if (*format == '%')
-			{
-				putchar('%');
-				printed_chars++;
-			}
+		} else {
+			putchar(*format); // Write the ordinary character to stdout
+			count++;
 		}
-		format++;
+		format++; // Move to the next character in the format string
 	}
 
 	va_end(args);
-
-	return printed_chars;
-=======
-        va_list args;
-        int printed_chars = 0, num = 0, len = 0;
-        char digit;
-
-        va_start(args, format);
-        while (*format)
-        {
-                if (*format != '%')
-                {
-                        write(1, format, 1);
-                        printed_chars++;
-                }
-                else
-                {
-                        format++;
-                        if (*format == 'd' || *format == 'i')
-                        {
-                                num = va_arg(args, int);
-                                if (num < 0)
-                                {
-                                        write(1, "-", 1);
-                                        num = -num;
-                                }
-                                do {
-                                        digit = '0' + num % 10;
-                                        write(1, &digit, 1);
-                                        num /= 10;
-                                        len++;
-                                } while (num > 0);
-                                printed_chars += len;
-                        }
-                }
-                format++;
-        }
-        va_end(args);
-        return (printed_chars);
->>>>>>> dbb07b5557d3053f015292bd6976a4904ddcb86b:task-1.c
+	return count;
 }
